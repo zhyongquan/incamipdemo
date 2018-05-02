4cff31eacc4dd7546b7e2c1cbfd912ac7a86c5d9
zhang yongquan <zhyqestudy@163.com>
2018-05-02 14:53:42 +0800

    Update mipdemo.m

--- mipdemo.m
+++ mipdemo.m
@@ -1,19 +1,21 @@
-%Establish the connection from MATLAB to INCA
+%% Open
+% Establish the connection from MATLAB to INCA
 IncaOpen;
-%Connects to the currently opened experiment
+% Connects to the currently opened experiment
 IncaOpenExperiment;
-%Add measure element in current group
+%% Measure element
+% Add measure element in current group
 IncaAddMeasureElement('WorkbaseDevice1',[],'B_GREEN');
 IncaAddMeasureElement('WorkbaseDevice1',[],'B_RED');
 IncaAddMeasureElement('WorkbaseDevice1',[],'B_YELLOW');
-%Start measure
+% Start measure
 data=[];
 time=[];
 IncaShowMessages(0);
 IncaSetMeasureReadMode(0)
 IncaStartMeasurement;
 deltaT=0;
-%Measure for 20 seconds
+% Measure for 20 seconds
 while(deltaT<20)
     pause(0.1)
     [t,d]=IncaGetRecords( 'WorkbaseDevice1','TimeC',500);
@@ -28,3 +30,13 @@ IncaStopMeasurement;
 IncaShowMessages(1);
 % Plot the results
 plot(time, data);
+%% Calibration element
+% Add calibration element
+IncaAddCalibrationElement('WorkbaseDevice1','DEMO_CONSTANT_1');
+% Get calibration value
+value=IncaGetCalibrationValue('WorkbaseDevice1','DEMO_CONSTANT_1');
+% Set calibration value
+DEMO_CONSTANT_1=10;
+result=IncaSetCalibrationValue('WorkbaseDevice1','DEMO_CONSTANT_1',DEMO_CONSTANT_1);
+%% Close
+IncaClose;
